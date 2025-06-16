7c489f29ed22aaad5e453b558ff89bf6e364acd3
psmey <psmey-dev@protonmail.com>
2025-06-16 18:33:05 +0200

    fix some more spelling

--- scripts/update_mod_folder.sh
+++ scripts/update_mod_folder.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # This script replaces your locally installed mod files with your currently
 # written files, becuase you probably don't want to directly work inside
-# the local mod folder with the whole repo
-# Usage:
-#   .
+# the local mod folder with the whole repo.
+# Just run it at the root folder of your repo.
 
-# adjust to your set-up if needed
+# Adjust to your set-up if needed
 LOCAL_MOD_PATH="/mnt/c/Program Files (x86)/Steam/steamapps/workshop/content/1158310/3479625504"
 
 if [ -z "$LOCAL_MOD_PATH" ]; then
     echo "❌ Variable LOCAL_MOD_PATH is not set"
 fi
 
-echo "🗑️ Removing previous mod file contents at"
+echo "🗑️ Removing previous mod file contents at:"
 echo "   $LOCAL_MOD_PATH"
 
 if [ -d "$LOCAL_MOD_PATH" ]; then
